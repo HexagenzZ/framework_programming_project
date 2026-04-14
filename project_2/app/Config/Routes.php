a3e28ebc3aa8bd365e639499b8c6fc585926b9c3
HexagenzZ <mzchevalier5@gmail.com>
2026-04-14 21:29:11 +0700

    feat: Add layouting and customization 404 page

--- project_2/app/Config/Routes.php
+++ project_2/app/Config/Routes.php
@@ -6,3 +6,9 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/portfolio', 'Portfolio::index');
+$routes->get('/about', 'About::index');
+
+$routes->set404Override(function() {
+ echo view('errors/not_found');
+});
