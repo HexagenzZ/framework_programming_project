867b6bc22e01a587de09f690cef2d05cb7821745
HexagenzZ <mzchevalier5@gmail.com>
2026-04-25 10:49:58 +0700

    feat: Add views create for admin

--- project_3/app/Database/Migrations/2026-04-18-034843_CreatePostsTable.php
+++ project_3/app/Database/Migrations/2026-04-18-034843_CreatePostsTable.php
@@ -1,50 +1,53 @@
 <?php
+
 namespace App\Database\Migrations;
+
 use CodeIgniter\Database\Migration;
 
 class CreatePostsTable extends Migration
 {
     public function up()
     {
-        $this->forge->addField([
-            'id' => [
-                'type'           => 'INT',
-                'constraint'     => 5,
-                'unsigned'       => true,
-                'auto_increment' => true
-            ],
-            'title' => [
-                'type'       => 'VARCHAR',
-                'constraint' => '255'
-            ],
-            'author' => [
-                'type'       => 'VARCHAR',
-                'constraint' => 100,
-                'default'    => 'John Doe',
-            ],
-            'content' => [
-                'type' => 'TEXT',
-                'null' => true,
-            ],
-            'status' => [
-                'type'       => 'ENUM',
-                'constraint' => ['published', 'draft'],
-                'default'    => 'draft',
-            ],
-            'slug' => [
-                'type'       => 'VARCHAR',
-                'constraint' => 100,
-                'null'       => true,
-            ],
+        // Membuat kolom/field untuk tabel posts
+		$this->forge->addField([
+			'id'          => [
+				'type'           => 'INT',
+				'constraint'     => 5,
+				'unsigned'       => true,
+				'auto_increment' => true
+			],
+			'title'       => [
+				'type'           => 'VARCHAR',
+				'constraint'     => '255'
+			],
+			'author'      => [
+				'type'           => 'VARCHAR',
+				'constraint'     => 100,
+				'default'        => 'John Doe',
+			],
+			'content' => [
+				'type'           => 'TEXT',
+				'null'           => true,
+			],
+			'status'      => [
+				'type'           => 'ENUM',
+				'constraint'     => ['published', 'draft'],
+				'default'        => 'draft',
+			],
             'created_at DATETIME DEFAULT CURRENT_TIMESTAMP'
-        ]);
+		]);
 
-        $this->forge->addKey('id', TRUE);
-        $this->forge->createTable('posts', TRUE);
+		// Membuat primary key
+		$this->forge->addKey('id', TRUE);
+
+		// Membuat tabel posts
+		$this->forge->createTable('posts', TRUE);
+
     }
 
     public function down()
     {
-        $this->forge->dropTable('posts');
+        // menghapus tabel posts
+		$this->forge->dropTable('posts');
     }
 }
