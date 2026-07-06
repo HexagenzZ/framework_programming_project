2573a2a207196f5ff71021989d15695a5152f8dd
HexagenzZ <mzchevalier5@gmail.com>
2026-07-07 02:21:35 +0700

    feat(project6): create Comment model, migrations, and Django Admin config

--- django/project6/blog/admin.py
+++ django/project6/blog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Post
+from .models import Comment, Post
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -11,4 +11,20 @@ class PostAdmin(admin.ModelAdmin):
     )
 
 
+class CommentAdmin(admin.ModelAdmin):
+    list_display = (
+        "post",
+        "author",
+        "body",
+        "created_at",
+    )
+    list_filter = ("created_at", "author")
+
+
 admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
+
+# Customize Admin branding for a premium look
+admin.site.site_header = "Digitalecture Blog Admin"
+admin.site.site_title = "Digitalecture Admin Portal"
+admin.site.index_title = "Welcome to Digitalecture Blog Management Portal"
--- django/project6/blog/models.py
+++ django/project6/blog/models.py
@@ -21,3 +21,13 @@ class Post(models.Model):
         import math
         word_count = len(self.body.split())
         return max(1, math.ceil(word_count / 200))
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey("auth.User", on_delete=models.CASCADE)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.author.username} on {self.post.title}"
